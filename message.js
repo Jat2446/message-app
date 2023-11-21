7fd91c95823594ac31055c43bad121c926406430
Jat2446 <109897160+Jat2446@users.noreply.github.com>
2023-11-22 00:19:35 +0530

    Add files via upload
    using express js

--- message.js
+++ message.js
@@ -1,19 +1,15 @@
-const http = require('http')
-
-const server = http.createServer((req,res) => {
-    const url = req.url;
-    if (url === "/"){
-        res.write("<html>");
-        res.write("<head><title>Enter Message</title></head>");
-        res.write('<body><form action="/message method="POST><input type ="text"><button type="submit">send</button</form</body>');
-        res.write("</html>")
-        return res.end();
-    }
-    res.setHeader("Content-Type","text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-    res.write("</html>")
-    res.end();
+// const http = require('http')
+// const routes = require('./route')
+const express = require('express')
+// console.log(routes.someText)
+const app = express();
+app.use((req,res,next) => {
+    console.log("i am middleware")
+    next();
 })
-server.listen(5000);
+app.use((req,res,next) => {
+    console.log("I am another midleware")
+    res.send("<h1> hello from express !</h1>")
+})
+// const server = http.createServer(app)
+app.listen(5000);
